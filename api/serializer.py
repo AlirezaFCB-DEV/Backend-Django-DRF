df87e23f8c78873970f0aa3dbdeecba42d9cfaac
AlirezaFCB-DEV <alirezadevloperui@gmail.com>
2025-11-20 15:32:32 +0330

    Del : Delete Api App For Better Tutorial

--- api/serializer.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Profile
-
-class Profile_Serializer(serializers.ModelSerializer) :
-    class Meta :
-        model = Profile
-        fields = ["id" , "user" , "bio" , "age"]
-        
-        
