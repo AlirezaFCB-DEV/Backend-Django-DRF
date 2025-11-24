f444a612b96083c5e1e5c3935bf99fe2aa59735f
AlirezaFCB-DEV <alirezadevloperui@gmail.com>
2025-11-24 12:20:18 +0330

    feat  : Using generic class-based views

--- Snippets/views.py
+++ Snippets/views.py
@@ -1,31 +1,15 @@
-from rest_framework import mixins, generics
+from rest_framework import generics
 from .models import Snippet
 from .serializer import Snippet_Serializer
 
 # Create your views here.
 
 
-class Snippet_List(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
-    queryset = Snippet.objects.all()
-    serializer_class = Snippet_Serializer
-    def get(self, req):
-        return self.list(req)
-
-    def post(self, req):
-        return self.create(req)
-
-
-class Snippet_Detail(mixins.RetrieveModelMixin,mixins.UpdateModelMixin , mixins.DestroyModelMixin, generics.GenericAPIView):
+class Snippet_List(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = Snippet_Serializer
 
-    def get(self, req , *args, **kwargs):
-        return self.retrieve(req , *args , **kwargs)
 
-    def put(self, req, *args , **kwargs):
-        return self.update(req , *args , **kwargs)
-        
-
-    def delete(self, req , *args , **kwargs):
-        return self.destroy(req , *args , **kwargs)
-        
+class Snippet_Detail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = Snippet_Serializer
