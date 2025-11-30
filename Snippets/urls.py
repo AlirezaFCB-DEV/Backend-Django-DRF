0323305e9b4dbca7fae9c9871b4b33cc4a6ed43c
AlirezaFCB-DEV <alirezadevloperui@gmail.com>
2025-11-30 16:27:20 +0330

    feat : routing routes with router in snippet/urls

--- Snippets/urls.py
+++ Snippets/urls.py
@@ -1,20 +1,17 @@
-from django.urls import path
-from rest_framework.urlpatterns import format_suffix_patterns
+from django.urls import path , include
+from rest_framework.routers import DefaultRouter
 from . import views
 
 app_name = "Snippets"
-snippet_list = views.Snippet_ViewSet.as_view({"get": "list", "post": "create"})
-snippet_detail = views.Snippet_ViewSet.as_view({"get": "retrieve", "put": "update", "patch": "partial_update", "delete": "destroy"})
+router = DefaultRouter()
+router.register(r"snippets" , views.Snippet_ViewSet)
 
 urlpatterns = [
     # path("snippets/",
     #      views.Snippet_List.as_view({"get": "list", "post": "create"})),
     # path("snippets/<int:pk>/", views.Snippet_Detail.as_view(
     #     {"get": "retrieve", "put": "update", "patch": "partial_update", "delete": "destroy"})), #! Bad CODE
-    path("snippets/" , snippet_list , name="snippet-list"),
-    path("snippets/<int:pk>/", snippet_detail, name="snippet-detail"),
+    path("" , include(router.urls)),
     path("users/", views.User_List.as_view()),
     path("users/<int:pk>/", views.User_Detail.as_view()),
 ]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
