84f28e508a538c2cf4cea15faa74427c7ea02464
AlirezaFCB-DEV <alirezadevloperui@gmail.com>
2025-11-30 16:59:48 +0330

    update : refactore routes

--- Snippets/urls.py
+++ Snippets/urls.py
@@ -5,13 +5,8 @@ from . import views
 app_name = "Snippets"
 router = DefaultRouter()
 router.register(r"snippets" , views.Snippet_ViewSet)
+router.register(r"users" , views.User_ViewSet)
 
 urlpatterns = [
-    # path("snippets/",
-    #      views.Snippet_List.as_view({"get": "list", "post": "create"})),
-    # path("snippets/<int:pk>/", views.Snippet_Detail.as_view(
-    #     {"get": "retrieve", "put": "update", "patch": "partial_update", "delete": "destroy"})), #! Bad CODE
     path("" , include(router.urls)),
-    path("users/", views.User_List.as_view()),
-    path("users/<int:pk>/", views.User_Detail.as_view()),
 ]
