a9e7dabf5420d7159f44f26bd79ec6640e1d1e25
AlirezaFCB-DEV <alirezadevloperui@gmail.com>
2025-12-01 16:21:52 +0330

    feat : api root for home page

--- Snippets/views.py
+++ Snippets/views.py
@@ -1,8 +1,18 @@
 from rest_framework import viewsets
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 from .models import Snippet
 from .serializer import Snippet_Serializer , User_Serializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from django.contrib.auth.models import User
+
+@api_view(["GET"])
+def api_root(req , format=None) :
+    return Response({
+        "users" : reverse("user-list" , request=req, format=format),
+        "snippets" : reverse("snippets-list" , request=req , format=format)
+    })
 
 class User_ViewSet(viewsets.ModelViewSet) :
     queryset  = User.objects.all()
@@ -12,7 +22,7 @@ class Snippet_ViewSet(viewsets.ModelViewSet) :
     queryset = Snippet.objects.all()
     serializer_class = Snippet_Serializer
     permission_classes = [IsAuthenticatedOrReadOnly]
-    
+
     def perform_create(self, serializer):
         return serializer.save(owner=self.request.user)
     
